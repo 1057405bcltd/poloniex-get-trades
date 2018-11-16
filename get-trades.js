596781e4801f362b56cd15b8d1eb9a3b0113821e
roderickmonk <roderickmonk@gmail.com>
2018-11-16 04:45:02 +0000

    whatever

--- get-trades.js
+++ get-trades.js
@@ -14,7 +14,7 @@ const assert = require("assert");
 const fs = require("fs-extra");
 const Json2csvParser = require("json2csv").Parser;
 const json2csvParser = new Json2csvParser({ header: false });
-const debug = require("debug");
+const debug = require("debug")("debug");
 const Poloniex = require("poloniex-api-node");
 const poloniex = new Poloniex(process.env.POLONIEX_API_KEY, process.env.POLONIEX_API_SECRET, {
     socketTimeout: 60000,
--- get-trades.ts
+++ get-trades.ts
@@ -6,7 +6,7 @@ const assert = require("assert");
 const fs = require("fs-extra");
 const Json2csvParser = require("json2csv").Parser;
 const json2csvParser = new Json2csvParser({ header: false });
-const debug = require("debug");
+const debug = require("debug")("debug");
 const Poloniex = require("poloniex-api-node");
 
 const poloniex = new Poloniex(
